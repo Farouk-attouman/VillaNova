0f253ce3ab0f2304f3aa5c88413010871dbbdbb1
faOum <attouman.farouk@gmail.com>
2026-07-01 15:41:23 +0200

    add config.js for github page deployment

---
+++ js/config.js
@@ -0,0 +1,11 @@
+window.VillaNova = window.VillaNova || {};
+
+VillaNova.config = {
+  // Supabase
+  supabaseUrl: 'https://vqnyrjxnsyndnvmxoqzr.supabase.co',
+  supabaseAnonKey: 'sb_publishable_BjjvJlNPGP1mT_ypqaAvYA_B_0yyr8F',
+
+  // OpenAgenda
+  openAgendaBase: 'https://api.openagenda.com/v2/agendas/69319016',
+  openAgendaKey: '9b8456d08b924cd68820a420f3360c85'
+};
